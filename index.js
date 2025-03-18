f189f6fc8c37d3fb7ae811e1cb1e48cd3aa919e8
judgeOstrich <jeraldnyaga15@gmail.com>
2025-03-19 00:51:13 +0300

    Finish arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 9;
+const num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2; // 62
+let random = Math.floor((Math.random() * 100) + 1);
+console.log(random)
+let mod = num3 % num4; // 4
+let max = Math.max(12,3,5,20); //20
